3f5ebaa24dfe79482fc4001b75ea7f99188a61bb
Dmitry Bardyshev <brd.dmitry@gmail.com>
2023-11-26 21:16:59 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
-      .setTitle('API')
-      .setVersion('1.0')
-      .addTag('api')
-      .build();
+    .setTitle('API')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
