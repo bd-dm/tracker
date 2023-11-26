83aecb8d9397a808865ea8d1b020c9da8eb06131
Dmitry Bardyshev <brd.dmitry@gmail.com>
2023-11-26 20:08:11 +0300

    swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 import { StoresModule } from './stores/stores.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [StoresModule, ProductsModule],
-  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,15 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateProductDto {
+    @ApiProperty()
     name: string
+
+    @ApiProperty()
     url: string
+
+    @ApiProperty()
     selector: string
+
+    @ApiProperty()
     storeId: string
 }
