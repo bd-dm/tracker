9d32422388eaa0cf527ccf87e528d66337734047
Dmitry Bardyshev <brd.dmitry@gmail.com>
2023-12-02 22:17:11 +0300

    workspaces

--- packages/api/src/stores/stores.module.ts
+++ packages/api/src/stores/stores.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StoresService } from './stores.service';
 import { StoresController } from './stores.controller';
-import {PrismaService} from "../prisma/prisma.service";
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [StoresController],
